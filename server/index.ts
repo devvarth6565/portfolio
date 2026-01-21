7c13ede1f911768588571001e5787a0938117e8d
devvarth Singh <devvarthsinghwork@gmail.com>
2026-01-21 09:23:54 +0530

    done with the setup of my portfolio database and other things

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
+
+
 const app = express();
 const httpServer = createServer(app);
 
